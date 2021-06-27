37973b15bde966e8f7470aa177dd845825a121ce
Amanda <amandacno@cos.ufrj.br>
2021-06-27 09:54:40 -0300

    Submission bug fix

--- create_sub.sh
+++ create_sub.sh
@@ -3,28 +3,33 @@
 # Executable to complete sub files. Must be edited for each use
 
 
-file=completeNLL.sub
+filename=completeNLL
+baseId=complete
 
-
-K_VAL=(1 2 5 10 20 100)
+K_VAL=(5 10 20 100)
 BAS_Size=4
 ITER=20000
 REPEAT=25
 
+basePath=$PWD
+file=result/${baseId}/${filename}.sub
+
+
+echo -e "###############################\n# ${baseId} net NLL eval\n###############################\n" >> $file
+echo -e "Executable\t\t= ${basePath}/complete.exe" >> $file
+echo -e "Universe\t\t= vanilla" >> $file
+echo -e "should_transfer_files\t= IF_NEEDED" >> $file
+echo -e "when_to_transfer_output\t= ON_EXIT" >> $file
+echo -e "\n" >> $file
+
 
 for k in ${K_VAL[@]}
 do
-	echo -e "Arguments\t\t= \"\$(Step) \$(Path) \$(Step) $BAS_Size $k $ITER\"" >> $file
-	echo -e "Log\t\t\t= log/complete_CD-${k}_\$(Step).log" >> $file
-	echo -e "Error\t\t\t= error/complete_CD-${k}_\$(Step).err" >> $file
-	echo -e "Output\t\t\t= out/complete_CD-${k}_\$(Step).out" >> $file
-
-	echo -n -e "transfer_output_files\t= \$(Path)nll_progress_complete_k${k}-run0.csv" >> $file
-	for ((idx=1; idx<$REPEAT; idx++)) 
-	do
-		echo -n -e " \$(Path)nll_progress_complete_k${k}-run${idx}.csv" >> $file
-	done
-	echo -e "\n" >> $file
+	echo -e "Arguments\t\t= \"\$(Step) . \$(Step) $BAS_Size $k $ITER\"" >> $file
+	echo -e "Log\t\t\t= ${basePath}/log/${baseId}_CD-${k}.log" >> $file
+	echo -e "Error\t\t\t= ${basePath}/error/${baseId}_CD-${k}_\$(Step).err" >> $file
+	echo -e "Output\t\t\t= ${basePath}/out/${baseId}_CD-${k}_\$(Step).out" >> $file
+	echo -e "transfer_output_files\t= nll_progress_${baseId}_k${k}-run\$(Step).csv" >> $file
 
 	echo -e "Queue $REPEAT" >> $file
 	echo -e "" >> $file
