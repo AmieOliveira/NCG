e045930383045c67874a3ef00ee96b82a1b7907d
Amanda <amandacno@cos.ufrj.br>
2021-07-03 15:53:02 -0300

    BAScon sub creation

--- create_sub.sh
+++ create_sub.sh
@@ -3,18 +3,19 @@
 # Executable to complete sub files. Must be edited for each use
 
 
-filename=BASconnectNLL
-baseId=BAScon
+filename=lRate05
+baseId=complete
 
 K_VAL=(1 2 5 10 20 100)
 BAS_Size=4
-ITER=6000
+ITER=10000
 REPEAT=25
 # V_VAL=(12 8 6 4)
-VERSIONS=(1 2)
+# VERSIONS=(1 2)
 
 basePath=$PWD
-file=result/${baseId}/${filename}.sub
+# file=result/${baseId}/${filename}.sub
+file=result/lRate05/${filename}.sub
 
 
 echo -e "###############################\n# ${baseId} net NLL eval\n###############################\n" >> $file
@@ -27,16 +28,16 @@ echo -e "\n" >> $file
 
 for k in ${K_VAL[@]}
 do
-	for v in ${VERSIONS[@]}
-	do
-		echo -e "Arguments\t\t= \"\$(Step) . \$(Step) $BAS_Size $k $ITER $v\"" >> $file
-		echo -e "Log\t\t\t= ${basePath}/log/bas${BAS_Size}_${baseId}${v}.log" >> $file
-		echo -e "Error\t\t\t= ${basePath}/error/bas${BAS_Size}_${baseId}${v}_CD-${k}_\$(Step).err" >> $file
-		echo -e "Output\t\t\t= ${basePath}/out/bas${BAS_Size}_${baseId}${v}_CD-${k}_\$(Step).out" >> $file
-		echo -e "transfer_output_files\t= nll_progress_bas${BAS_Size}_${baseId}V${v}_k${k}-run\$(Step).csv" >> $file
+	# for v in ${VERSIONS[@]}
+	# do
+		echo -e "Arguments\t\t= \"\$(Step) . \$(Step) $BAS_Size $k $ITER 1 5 0.05\"" >> $file
+		echo -e "Log\t\t\t= ${basePath}/log/bas${BAS_Size}_${baseId}.log" >> $file
+		echo -e "Error\t\t\t= ${basePath}/error/bas${BAS_Size}_${baseId}_CD-${k}_\$(Step).err" >> $file
+		echo -e "Output\t\t\t= ${basePath}/out/bas${BAS_Size}_${baseId}_CD-${k}_\$(Step).out" >> $file
+		echo -e "transfer_output_files\t= nll_progress_bas${BAS_Size}_${baseId}_k${k}-run\$(Step).csv" >> $file
 
 		echo -e "Queue $REPEAT" >> $file
 		echo -e "" >> $file
-	done
+	# done
 done
 
