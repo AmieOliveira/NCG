9efd38ae780bdcb3ec33366aba26de003e3d7bb7
Amanda <amandacno@cos.ufrj.br>
2021-07-27 11:33:29 -0300

    SGD connectivity optimization submission script

--- create_sub.sh
+++ create_sub.sh
@@ -3,18 +3,19 @@
 # Executable to complete sub files. Must be edited for each use
 
 
-filename=specialistV4
-baseId=BAScon
+filename=sgd_test
+baseId=SGD
 
 K_VAL=(1 2 5 10 20 100)
 BAS_Size=4
-ITER=10000
+ITER=20000
 REPEAT=25
 # V_VAL=(14 12 10 8 6 4)
 # V_TYPE=l
 # V_TYPE=s
-VERSIONS=(4)
-#(2 3 4)
+# VERSIONS=(2 3 4)
+LR_VAL=(0.1 0.01)
+P_VAL=(1 0.75 0.5)
 
 basePath=$PWD
 file=result/${baseId}/${filename}.sub
@@ -28,7 +29,7 @@ file=result/${baseId}/${filename}.sub
 # 	fi
 # fi
 
-echo -e "###############################\n# ${baseId} net NLL eval\n###############################\n" >> $file
+echo -e "###############################\n# ${baseId} opt NLL eval\n###############################\n" >> $file
 echo -e "Executable\t\t= ${basePath}/${baseId}.exe" >> $file
 echo -e "Universe\t\t= vanilla" >> $file
 echo -e "should_transfer_files\t= IF_NEEDED" >> $file
@@ -38,17 +39,20 @@ echo -e "\n" >> $file
 
 for k in ${K_VAL[@]}
 do
-	for v in ${VERSIONS[@]}  
-		# ${V_VAL[@]}
+	for lr in ${LR_VAL[@]}  
+		# ${V_VAL[@]} ${VERSIONS[@]}
 	do
-		echo -e "Arguments\t\t= \"\$(Step) . \$(Step) $BAS_Size $v $k $ITER 5 0.01 1\"" >> $file
-		echo -e "Log\t\t\t= ${basePath}/log/bas${BAS_Size}_${baseId}.log" >> $file
-		echo -e "Error\t\t\t= ${basePath}/error/bas${BAS_Size}_${baseId}${v}_CD-${k}_\$(Step).err" >> $file
-		echo -e "Output\t\t\t= ${basePath}/out/bas${BAS_Size}_${baseId}${v}_CD-${k}_\$(Step).out" >> $file
-		echo -e "transfer_output_files\t= nll_progress_bas${BAS_Size}_${baseId}V${v}_k${k}-run\$(Step).csv" >> $file
+		for p in ${P_VAL[@]}
+		do
+			echo -e "Arguments\t\t= \"\$(Step) . \$(Step) $BAS_Size $k $ITER 5 $lr $p 1\"" >> $file
+			echo -e "Log\t\t\t= ${basePath}/log/bas${BAS_Size}_${baseId}.log" >> $file
+			echo -e "Error\t\t\t= ${basePath}/error/bas${BAS_Size}_${baseId}_CD-${k}_lr${lr}_p${p}_\$(Step).err" >> $file
+			echo -e "Output\t\t\t= ${basePath}/out/bas${BAS_Size}_${baseId}${v}_CD-${k}_lr${lr}_p${p}_\$(Step).out" >> $file
+			echo -e "transfer_output_files\t= nll_bas${BAS_Size}_${baseId}_CD-${k}_lr${lr}_p${p}_run\$(Step).csv,connectivity_bas${BAS_Size}_${baseId}_CD-${k}_lr${lr}_p${p}_run\$(Step).csv" >> $file
 
-		echo -e "Queue $REPEAT" >> $file
-		echo -e "" >> $file
+			echo -e "Queue $REPEAT" >> $file
+			echo -e "" >> $file
+		done
 	done
 done
 
