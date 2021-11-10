cd85e9c9f8954080e9348ce987f6c2434a693c29
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2021-11-10 17:14:19 -0300

    Printer scripts' update. Bug fixes in connectivity printing

--- connectivity_printer.py
+++ connectivity_printer.py
@@ -7,32 +7,42 @@ from matplotlib import cm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Inputs ---------------
-basSize = 4
-k = 5
-p = 0.75
-seed = 103
-lrate = 0.1
+# basSize = 4
+k = 10
+p = 1
+H = 15
+seed = 40
+lrate = 0.01
+bsize = 50
+iters = 100
+
+cBatch = "singleBatch"
+timeScale = 20
+limiar = 0.04
 
 # inputfilename = f"Training Outputs/Teste SGD/connectivity_bas{basSize}_SGD_CD-{k}_p{p}_seed{seed}.csv"
-inputfilename = f"Training Outputs/Teste SGD/connectivity_bas{basSize}_SGD_CD-{k}_lr{lrate}_p{p}_run{seed-100}.csv"
-outpath = "Plots/Teste SGD"
+inputfilename = f"Training Outputs/Teste Gradiente/SingleBatch_scale20_lim0.04/connectivity_mnist_sgd-{p}_H{H}_CD-{k}_lr{lrate}_mBatch{bsize}_iter{iters}_seed{seed}_run0.csv"
+outpath = "Plots/Teste Gradiente"
 # ---------------
 
+size = 784  # basSize**2
 
 figSize = {"default": (6.7, 5), "wide": (13, 5)}
 
+extras = f"-{cBatch}-timeScale{timeScale}-limiar{limiar}"
 
 def read_input(filename):
-    size = basSize**2
     df = pd.read_csv(filename, comment="#", index_col=0, header=None)
     df = df.astype(int)
     arr = df.to_numpy()
-    return np.reshape(arr, (arr.shape[0],size,size))
+    return arr  # np.reshape(arr, (arr.shape[0], H, size))
 
 
 if __name__ == "__main__":
     conn = read_input(inputfilename)
     # print(conn[-1])  # Verificação parece ok!
+
+    X = size
 
     iterations = conn.shape[0]
     size = conn.shape[1]
@@ -46,79 +56,107 @@ if __name__ == "__main__":
     gx_med = np.zeros(iterations)
     gx_min = np.zeros(iterations)
 
-    for i in range(iterations):
-        gh = [sum(conn[i, r, :]) for r in range(size)]
-        gh_med[i] = sum(gh)/len(gh)
-        gh_min[i] = min(gh)
-        gh_max[i] = max(gh)
+    for itIdx in range(iterations):
+        xDegrees = np.zeros(X)
+        hDegrees = np.zeros(H)
 
-        gx = [sum(conn[i, :, c]) for c in range(size)]
-        gx_med[i] = sum(gx)/len(gx)
-        gx_min[i] = min(gx)
-        gx_max[i] = max(gx)
+        for i in range(H):
+            for j in range(X):
+                xDegrees[j] += conn[itIdx, X * i + j]
+                hDegrees[i] += conn[itIdx, X * i + j]
+
+        sumOfdegs = hDegrees[0]
+        gh_max[itIdx] = hDegrees[0]
+        gh_min[itIdx] = hDegrees[0]
+
+        for i in range(1, H):
+            sumOfdegs += hDegrees[i]
+
+            if hDegrees[i] < gh_min[itIdx]:
+                gh_min[itIdx] = hDegrees[i]
+            if hDegrees[i] > gh_max[itIdx]:
+                gh_max[itIdx] = hDegrees[i]
+
+        gh_med[itIdx] = float(sumOfdegs) / H
+
+        sumOfdegs = xDegrees[0]
+        gx_max[itIdx] = xDegrees[0]
+        gx_min[itIdx] = xDegrees[0]
+
+        for j in range(1, X):
+            sumOfdegs += xDegrees[j]
+
+            if xDegrees[j] < gx_min[itIdx]:
+                gx_min[itIdx] = xDegrees[j]
+            if xDegrees[j] > gx_max[itIdx]:
+                gx_max[itIdx] = xDegrees[j]
+
+        gx_med[itIdx] = float(sumOfdegs) / X
 
     fig, ax = plt.subplots(nrows=2, ncols=1, sharex='col', figsize=figSize["default"])
     fig.suptitle(f"Unit's degree for CD-{k}, p = {p}")
 
-    ax[0].plot(gh_max, label="Maximum")
-    ax[0].plot(gh_med, label="Mean")
-    ax[0].plot(gh_min, label="Minimum")
-    ax[0].set_ylabel("Degree of hidden unit")
+    x_axis = list(range(0, iters+1, timeScale))
+
+    ax[0].step(x_axis, gh_max, where='post', label="Maximum")
+    ax[0].step(x_axis, gh_med, where='post', label="Mean")
+    ax[0].step(x_axis, gh_min, where='post', label="Minimum")
+    ax[0].set_ylabel("Degree of hidden units")
     ax[0].grid(color="gray", linestyle=":", linewidth=.2)
     ax[0].legend(loc="upper right", prop={'size': 6})
 
-    ax[1].plot(gx_max, label="Maximum")
-    ax[1].plot(gx_med, label="Mean")
-    ax[1].plot(gx_min, label="Minimum")
-    ax[1].set_ylabel("Degree of visible unit")
+    ax[1].step(x_axis, gx_max, where='post', label="Maximum")
+    ax[1].step(x_axis, gx_med, where='post', label="Mean")
+    ax[1].step(x_axis, gx_min, where='post', label="Minimum")
+    ax[1].set_ylabel("Degree of visible units")
     ax[1].grid(color="gray", linestyle=":", linewidth=.2)
     ax[1].legend(loc="upper right", prop={'size': 6})
 
     plt.xlabel("Iteration")
 
     # plt.show()
-    plt.savefig(f"{outpath}/nodeDegree_bas{basSize}_SGD_CD-{k}_p{p}_seed{seed}.pdf", transparent=True)
+    plt.savefig(f"{outpath}/nodeDegree_mnist_SGD_CD-{k}_lr{lrate}_mBatch{bsize}_iter{iters}_seed{seed}{extras}.pdf", transparent=True)
 
 
-    # Edge activation graphs
-    activation = [[sum(conn[:, r, c])/float(iterations) for c in range(size)] for r in range(size)]
-
-    spacingX = 20
-    spacingY = 100
-
-    cmapname = "copper"  # "Greys"
-    cmap = cm.get_cmap(cmapname)
-
-    fig, ax = plt.subplots(1, figsize=figSize["wide"])
-    ax.set_aspect('equal')
-    plt.title(f"Activation pattern through training for CD-{k}, p = {p}")
-
-    for r in range(size):
-        for c in range(size):
-            plt.plot([r*spacingX, c*spacingX], [spacingY, 0], color=cmap(activation[r][c]), linewidth=1, alpha=0.85)
-
-    for i in range(size):
-        c_h = plt.Circle( (spacingX*i, spacingY), spacingX/4, color='grey', zorder=5 )
-        ax.add_patch(c_h)
-        plt.annotate(r"$h_{{ {} }}$".format(i), (spacingX*i, spacingY), ha="center", va="center", zorder=10)
-
-        c_x = plt.Circle( (spacingX*i, 0), spacingX/4, color='firebrick', zorder=5 )
-        ax.add_patch(c_x)
-        plt.annotate(r"$x_{{ {} }}$".format(i), (spacingX*i, 0), ha="center", va="center", zorder=10)
-
-    plt.tick_params(
-        axis='both',
-        which='major',
-        bottom=False,
-        left=False,
-        labelbottom=False,
-        labelleft=False
-    )
-
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    fig.colorbar(cm.ScalarMappable(cmap=cmapname), cax=cax)
-
-    # fig.tight_layout()
-    # plt.show()
-    plt.savefig(f"{outpath}/edgeActivation_bas{basSize}_SGD_CD-{k}_p{p}_seed{seed}.pdf", transparent=True)
+    # # Edge activation graphs
+    # activation = [[sum(conn[:, r, c])/float(iterations) for c in range(size)] for r in range(size)]
+    #
+    # spacingX = 20
+    # spacingY = 100
+    #
+    # cmapname = "copper"  # "Greys"
+    # cmap = cm.get_cmap(cmapname)
+    #
+    # fig, ax = plt.subplots(1, figsize=figSize["wide"])
+    # ax.set_aspect('equal')
+    # plt.title(f"Activation pattern through training for CD-{k}, p = {p}")
+    #
+    # for r in range(size):
+    #     for c in range(size):
+    #         plt.plot([r*spacingX, c*spacingX], [spacingY, 0], color=cmap(activation[r][c]), linewidth=1, alpha=0.85)
+    #
+    # for i in range(size):
+    #     c_h = plt.Circle( (spacingX*i, spacingY), spacingX/4, color='grey', zorder=5 )
+    #     ax.add_patch(c_h)
+    #     plt.annotate(r"$h_{{ {} }}$".format(i), (spacingX*i, spacingY), ha="center", va="center", zorder=10)
+    #
+    #     c_x = plt.Circle( (spacingX*i, 0), spacingX/4, color='firebrick', zorder=5 )
+    #     ax.add_patch(c_x)
+    #     plt.annotate(r"$x_{{ {} }}$".format(i), (spacingX*i, 0), ha="center", va="center", zorder=10)
+    #
+    # plt.tick_params(
+    #     axis='both',
+    #     which='major',
+    #     bottom=False,
+    #     left=False,
+    #     labelbottom=False,
+    #     labelleft=False
+    # )
+    #
+    # divider = make_axes_locatable(ax)
+    # cax = divider.append_axes("right", size="5%", pad=0.05)
+    # fig.colorbar(cm.ScalarMappable(cmap=cmapname), cax=cax)
+    #
+    # # fig.tight_layout()
+    # # plt.show()
+    # plt.savefig(f"{outpath}/edgeActivation_bas{basSize}_SGD_CD-{k}_p{p}_seed{seed}.pdf", transparent=True)
--- printerScratch.py
+++ printerScratch.py
@@ -7,26 +7,23 @@ from math import log
 
 # basSize = 4
 
-comparison = "compConvSGD"  # "inicializacaoA_"
-# "neighTypes", "neighbors10-14_BAScon2-4_complete"
-# "8neighbors-basConnect2-complete", "basConnectV1-completeH9-completeH16", "bSize&lRate"
-# "8&12neighbors_basConnect2_complete", "13neighbors-specialist-complete", "8&12neighbors_basConnect2_complete"
+comparison = "connectivityScale-batch1k-limiar0.12"
 
 # k_vals = [1, 10]  # [100, 20, 10, 5, 2, 1]
 # neighbors = [14, 12, 10]   # 16 [14, 12, 10, 8, 6, 4]
 # neighType = "spiral"      # "line", "spiral"
 # versions = [2, 3, 4]
 # identifier = 2
-k = 1
-lim_iter = 6000
+k = 10
+lim_iter = 100
 errorType = None        # None, "std", "quartile"
-repeat = 5
-p_vals = [1, 0.5]
-# p = 1
-seed = 1
+# repeat = 5
+# p_vals = [1, 0.5]
+p = 1
+seed = 40
 lRate = 0.01
 bSize = 50
-H = 16
+H = 15
 addOthers = False
 zoom = False
 
@@ -37,22 +34,117 @@ sizeNum = {1: 1, 2: 1, 5: 1, 10: 2, 20: 2, 100: 3}
 
 fig, ax = plt.subplots(1, figsize=figSize[plotSize])
 
-# NLL truncation error plot ---------
-filename = f"Training Outputs/Teste MNIST/nll_mnist_complete_H16_CD-{k}_lr0.01_mBatch50_iter{lim_iter}_run{seed}.csv"
+## NLL truncation error plot ---------
+
+# filename = f"Training Outputs/Teste Gradiente/BatchSize50/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Batch 50"})
+# df[f"Batch 50"].plot(ax=ax, linewidth=1, alpha=0.8)
+
+# filename = f"Training Outputs/Teste Gradiente/Batch1k_scale2/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Time Scale 2"})
+# df[f"Time Scale 2"].plot(ax=ax, linewidth=1, alpha=0.8)
+#
+# filename = f"Training Outputs/Teste Gradiente/BatchSize1k/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# # df = df.rename(columns={"NLL": f"Batch 1k"})
+# # df[f"Batch 1k"].plot(ax=ax, linewidth=1, alpha=0.8)
+# df = df.rename(columns={"NLL": f"Time Scale 5"})
+# df[f"Time Scale 5"].plot(ax=ax, linewidth=1, alpha=0.8)
+#
+# filename = f"Training Outputs/Teste Gradiente/Batch1k_scale10/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Time Scale 10"})
+# df[f"Time Scale 10"].plot(ax=ax, linewidth=1, alpha=0.8)
+
+# filename = f"Training Outputs/Teste Gradiente/Batch1k_scale20/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# # df = df.rename(columns={"NLL": f"Time Scale 20"})
+# # df[f"Time Scale 20"].plot(ax=ax, linewidth=1, alpha=0.8)
+# df = df.rename(columns={"NLL": f"Threshold 0,04"})
+# df[f"Threshold 0,04"].plot(ax=ax, linewidth=1, alpha=0.8)
+#
+# filename = f"Training Outputs/Teste Gradiente/Batch1k_scale20_lim0.08/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Threshold 0,08"})
+# df[f"Threshold 0,08"].plot(ax=ax, linewidth=1, alpha=0.8)
+#
+filename = f"Training Outputs/Teste Gradiente/Batch1k_scale20_lim0.12/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
 df = pd.read_csv(filename, comment="#", index_col=0)
 df = df.astype(float)
-df = df.iloc[0:lim_iter]
+df = df.iloc[0:lim_iter+1]
 
-df = df.rename(columns={"NLL": f"Real Value"})
-df[f"Real Value"].plot(ax=ax, linewidth=1, alpha=0.8)
+# df = df.rename(columns={"NLL": f"Threshold 0,12"})
+# df[f"Threshold 0,12"].plot(ax=ax, linewidth=1, alpha=0.8)
+df = df.rename(columns={"NLL": f"Scale 20"})
+df[f"Scale 20"].plot(ax=ax, linewidth=1, alpha=0.8)
 
-filename = f"Training Outputs/Teste MNIST/nll_mnist_complete_H16_CD-{k}_lr0.01_mBatch50_iter{lim_iter}_run-truncated{seed}.csv"
+filename = f"Training Outputs/Teste Gradiente/Batch1k_scale5_lim0.12/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
 df = pd.read_csv(filename, comment="#", index_col=0)
 df = df.astype(float)
-df = df.iloc[0:lim_iter]
+df = df.iloc[0:lim_iter+1]
 
-df = df.rename(columns={"NLL": f"Truncated Estimate"})
-df[f"Truncated Estimate"].plot(ax=ax, linewidth=1, alpha=0.8)
+df = df.rename(columns={"NLL": f"Scale 5"})
+df[f"Scale 5"].plot(ax=ax, linewidth=1, alpha=0.8)
+
+# filename = f"Training Outputs/Teste Gradiente/SingleBatch/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Single Batch"})
+# df[f"Single Batch"].plot(ax=ax, linewidth=1, alpha=0.8)
+
+# filename = f"Training Outputs/Teste Gradiente/SingleBatch_scale20_lim0.04/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Threshold 0,04"})
+# df[f"Threshold 0,04"].plot(ax=ax, linewidth=1, alpha=0.8)
+#
+# filename = f"Training Outputs/Teste Gradiente/SingleBatch_scale20_lim0.005/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Threshold 0,005"})
+# df[f"Threshold 0,005"].plot(ax=ax, linewidth=1, alpha=0.8)
+#
+# filename = f"Training Outputs/Teste Gradiente/SingleBatch_scale20_lim0.001/nll_mnist_sgd-{p}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+# df = pd.read_csv(filename, comment="#", index_col=0)
+# df = df.astype(float)
+# df = df.iloc[0:lim_iter+1]
+#
+# df = df.rename(columns={"NLL": f"Threshold 0,001"})
+# df[f"Threshold 0,001"].plot(ax=ax, linewidth=1, alpha=0.8)
+
+filename = f"Training Outputs/Teste Gradiente/nll_mnist_complete_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}_run0.csv"
+df = pd.read_csv(filename, comment="#", index_col=0)
+df = df.astype(float)
+df = df.iloc[0:lim_iter+1]
+
+df = df.rename(columns={"NLL": f"Traditional Training"})
+df[f"Traditional Training"].plot(ax=ax, linewidth=1, alpha=0.8, color="gray")
 
 plt.title(f"NLL evolution through complete RBM training")
 # ---------
@@ -450,6 +542,7 @@ plt.xlabel("Epoch")
 plt.ylabel("Average NLL")
 plt.grid(color="gray", linestyle=":", linewidth=.2)
 # plt.xlim(-10, lim_iter+10)
+plt.ylim(150, 250)
 plt.legend()
 
 # # Lower limit of NLL
@@ -457,4 +550,4 @@ plt.legend()
 # limitante = - log(1.0/nSamples)
 # plt.plot([0, lim_iter], [limitante, limitante], "r--")
 
-plt.savefig(f"Plots/Teste MNIST/nll_mnist_complete_truncation_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}.pdf", transparent=True)
+plt.savefig(f"Plots/Teste Gradiente/nll_mnist_{comparison}_H{H}_CD-{k}_lr{lRate}_mBatch{bSize}_iter{lim_iter}_seed{seed}.pdf", transparent=True)
