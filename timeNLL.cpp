848c208becc099c5062f7bf17c001e884984b721
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2021-11-18 16:43:21 -0300

    (minor) NLL values analysis

--- timeNLL.cpp
+++ timeNLL.cpp
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
     // Traditional RBM
     RBM model;
 
-    model.load("Training Outputs/Redes Treinadas/H25_nets/mnist_sgd-1_H25_CD-1_lr0.01_mBatch50_iter200_run0.rbm");
+    model.load("Training Outputs/Redes Treinadas/H500_nets/mnist_sgd-1_H500_CD-1_lr0.01_mBatch50_iter200_run0.rbm");
 
     cout << "Training model for " << iter << " epochs" << endl;
 
@@ -52,46 +52,46 @@ int main(int argc, char **argv) {
         // // model.connectivity(true); model.optSetup(Heuristic::SGD, p); model.fit_connectivity(bas);
 
         auto t1 = chrono::steady_clock::now();
-        cout << "Exact value (through H): " << model.negativeLogLikelihood(bas, ZEstimation::None_H) << endl;
+        // cout << "Exact value (through H): " << model.negativeLogLikelihood(bas, ZEstimation::None_H) << endl;
         auto t2 = chrono::steady_clock::now();
         // cout << "Truncation estimation (no verification): " << model.negativeLogLikelihood(bas, ZEstimation::Trunc) << endl;
-        auto t3 = chrono::steady_clock::now();
+        // auto t3 = chrono::steady_clock::now();
 
-        cout << "Time elapsed for total calculation was " << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count()/1000
-             << " ms and for truncated Z was " << chrono::duration_cast<chrono::milliseconds>(t3 - t2).count()/1000 << " s" << endl;
-        // cout << "\t\t(In nanoseconds truncation calculated " <<  chrono::duration_cast<chrono::microseconds>(t3 - t2).count() << ")" << endl;
+        // cout << "Time elapsed for total calculation was " << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count()/1000
+        //      << " ms and for truncated Z was " << chrono::duration_cast<chrono::milliseconds>(t3 - t2).count()/1000 << " s" << endl;
+        // // cout << "\t\t(In nanoseconds truncation calculated " <<  chrono::duration_cast<chrono::microseconds>(t3 - t2).count() << ")" << endl;
 
-        // vector<double> vals;
-        // vector<double> times;
-        //
-        // double logZ_AIS[] = {253.794647};
-        // double time_logZ[] = {75.559418};
-        //
-        // double sumAIS = 0, sumTimes = 0, tmp, tmpT;
-        //
-        // for (int r=1; r <= repeat; r++) {
-        //     model.setRandomSeed(seed + s + r);
-        //
-        //     t1 = chrono::steady_clock::now();
-        //     tmp = model.negativeLogLikelihood(bas, logZ_AIS[r-1]);
-        //     t2 = chrono::steady_clock::now();
-        //     // tmpT = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
-        //     tmpT = time_logZ[r-1] + chrono::duration_cast<chrono::milliseconds>(t2 - t1).count()/1000;
-        //
-        //     sumAIS += tmp;
-        //     sumTimes += tmpT;
-        //     vals.push_back(tmp);
-        //     times.push_back(tmpT);
-        //     cout << "AIS estimation " << r << ": " << tmp << " (took " << tmpT << " s)" << endl;
-        // }
-        // sumAIS = sumAIS/repeat;
-        // sumTimes = sumTimes/repeat;
-        // double std = 0, stdT = 0;
-        // for (auto v: vals) { std += pow(sumAIS - v, 2); }
-        // for (auto t: times) { stdT += pow(sumTimes - t, 2); }
-        // std = sqrt(std/repeat);
-        // stdT = sqrt(stdT/repeat);
-        // cout << "\tAIS mean: " << sumAIS << " ± " << std << "; time to calculate is " << sumTimes << " ± " << stdT << " s" << endl;
-        // cout << endl;
+        vector<double> vals;
+        vector<double> times;
+
+        double logZ_AIS[] = {717.842878};
+        double time_logZ[] = {177.524293};
+
+        double sumAIS = 0, sumTimes = 0, tmp, tmpT;
+
+        for (int r=1; r <= repeat; r++) {
+            model.setRandomSeed(seed + s + r);
+
+            t1 = chrono::steady_clock::now();
+            tmp = model.negativeLogLikelihood(bas, logZ_AIS[r-1]);
+            t2 = chrono::steady_clock::now();
+            // tmpT = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+            tmpT = time_logZ[r-1] + chrono::duration_cast<chrono::milliseconds>(t2 - t1).count()/1000;
+
+            sumAIS += tmp;
+            sumTimes += tmpT;
+            vals.push_back(tmp);
+            times.push_back(tmpT);
+            cout << "AIS estimation " << r << ": " << tmp << " (took " << tmpT << " s)" << endl;
+        }
+        sumAIS = sumAIS/repeat;
+        sumTimes = sumTimes/repeat;
+        double std = 0, stdT = 0;
+        for (auto v: vals) { std += pow(sumAIS - v, 2); }
+        for (auto t: times) { stdT += pow(sumTimes - t, 2); }
+        std = sqrt(std/repeat);
+        stdT = sqrt(stdT/repeat);
+        cout << "\tAIS mean: " << sumAIS << " ± " << std << "; time to calculate is " << sumTimes << " ± " << stdT << " s" << endl;
+        cout << endl;
     }
 }
